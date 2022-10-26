63fde5bba1f3ed789a61288bdabee1368a7dab90
hyojine <touroz@naver.com>
2022-10-26 09:57:14 +0900

    1026test draft

---
+++ articles/views.py
@@ -0,0 +1,21 @@
+from rest_framework.response import Response
+from .serializers import ArticleSerializer
+from rest_framework.decorators import api_view
+# from .models import Article
+
+# Create your views here.
+@api_view(('GET','POST'))
+def index(request):
+    if request.method == 'GET':
+        articles = request.objects.all()
+        serializer = ArticleSerializer(articles,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        # article = Article()
+        # article.title = request.POST.get('title','')
+        # article.content = request.POST.get('content','')
+        # article.save()
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
